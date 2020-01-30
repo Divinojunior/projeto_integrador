2a88872fcb19fd5decaef66033788efc1065c833
Divinojunior <divino.soares@gmail.com>
2020-01-30 16:22:24 -0300

    Add files via upload

--- integrador.py
+++ integrador.py
@@ -1,5 +1,5 @@
 #pip freeze > requirements.txt
-#comment
+
 import os
 import streamlit as st 
 from PIL import Image
@@ -32,7 +32,7 @@ def main():
 		visualize_data()
 	elif page == "Business":
 		st.title("Calculando a probabilidade do sócio sair")
-		predicao()
+		#predicao()
 
 def visualize_data():
 
@@ -154,87 +154,6 @@ def visualize_data():
 			st.write(cust_plot)
 			st.pyplot()
 
-def predicao():
-	df=pd.read_csv('./Datasets/SOCIOS_USO_CONCATL.csv', encoding='latin-1', delimiter = ';')
-
-
-	#st.sidebar.slider('slider',1,10)
-
-	#st.multiselect('multiselect', [1,2,3])
-
-	#st.sidebar.selectbox('selectbox',[1,2,3,4])
-	meses_assoc = st.sidebar.number_input('Qnt Meses Associação', value=1, min_value = 0, max_value = 1000, step=1)
-
-	pagos_dia = st.sidebar.number_input('Qnt Boletos Pagos em Dia', value=1, min_value = 0, max_value = 1000, step=1)
-
-	pago_atraso = st.sidebar.number_input('Qnt Boletos Pagos Atrasados', value=1, min_value = 0, max_value = 1000, step=1)
-
-	abertos = st.sidebar.number_input('Qnt Boletos Abertos', value=1, min_value = 0, max_value = 1000, step=1)
-
-	cargo1 = st.sidebar.number_input('Qnt Participações - Cargo 1', value=1, min_value = 0, max_value = 1000, step=1)
-
-	cargo2 = st.sidebar.number_input('Qnt Participações - Cargo 2', value=1, min_value = 0, max_value = 1000, step=1)
-
-	cargo3 = st.sidebar.number_input('Qnt Participações - Cargo 3', value=1, min_value = 0, max_value = 1000, step=1)
-
-	total_part = st.sidebar.number_input('Qnt Total de Participações', value=1, min_value = 0, max_value = 1000, step=1)
-
-	meses_sem_part = st.sidebar.number_input('Qnt Meses sem Participações', value=1, min_value = 0, max_value = 1000, step=1)
-
-
-	#st.sidebar.date_input("date_input", date(2020, 1, 20))
-
-	#st.sidebar.checkbox('checkbox')
-
-	st.write(df)
-
-	csv = df.to_csv(index=False)
-	b64 = base64.b64encode(csv.encode()).decode() 
-	href = f'<a href="data:file/csv;base64,{b64}" download="export.csv">Exportar df</a>'
-	st.markdown(href, unsafe_allow_html=True)
-
-	import numpy as np
-	#image = Image.open('img.png')
-	#st.image(image)
-	#X_new = np.array(meses_assoc, pagos_dia, pago_atraso, abertos, cargo1, cargo2, cargo3, total_part, meses_sem_part)
-	X_new = ([[meses_assoc, pagos_dia, pago_atraso, abertos, cargo1, cargo2, cargo3, total_part, meses_sem_part]])
-	X =  pd.DataFrame(df.drop(["COD_EMPRESA","COD_FILIAL","B_PEDIDO_DEMISSAO","B_SOCIO","GRUPO1","GRUPO2","GRUPO3","GRUPO4","GRUPO5","GRUPO6","GRUPO7","GRUPO8","GRUPO9","GRUPO10","GRUPO11","PORTE","PAGO_EM_DIA_POSTERGADO"],axis = 1),)
-	y = df.B_PEDIDO_DEMISSAO
-
-	from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-
-	from sklearn import tree
-	# instanciando e ajustando o modelo
-
-	#clf = tree.DecisionTreeClassifier(criterion='gini',max_depth=9,min_samples_split=50)
-	#clf = clf.fit(X,y)
-
-	clf_2 = GradientBoostingClassifier(learning_rate= 0.1, max_depth= 6, n_estimators= 50)
-	clf_2 = clf_2.fit(X,y)
-
-
-
-	#predictions_DT = clf.predict_proba(X_new)
-	predictions_GB = clf_2.predict_proba(X_new)
-
-
-	#st.write(predictions_DT)
-	st.write("PROBABILIDADE DO SOCIO SAIR ",round(predictions_GB[0][1]*100,2),'%')
-	#st.write(round(predictions_GB[0][1],2),'%')
-	#st.write(predictions_GB)
-	#st.write(X_new)
-	import shap  # package used to calculate Shap values
-
-	# Create object that can calculate shap values
-	explainer = shap.TreeExplainer(rfc)
-
-	# calculate shap values. This is what we will plot.
-	# Calculate shap_values for all of val_X rather than a single row, to have more data for plot.
-	shap_values = explainer.shap_values(X)
-
-	# Make plot. Index of [1] is explained in text below.
-	shap.summary_plot(shap_values[1], X)
-	shap.summary_plot(shap_values, X, plot_type='bar')
 
 if __name__ == '__main__':
 	main()
