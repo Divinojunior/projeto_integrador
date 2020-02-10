1f623185a3609944791132ffc3f86e1d8338f3d2
Divinojunior <divino.soares@gmail.com>
2020-02-10 17:25:47 -0300

    Add files via upload

--- integrador.py
+++ integrador.py
@@ -1,5 +1,6 @@
-#pip freeze > requirements.txt (deletar )
+#pip freeze > requirements.txt (deletar pywin32==227)
 #streamlit run integrador.py
+# F1 'Convert Identations to Tab'
 
 import os
 import streamlit as st 
@@ -26,14 +27,17 @@ def main():
 	
 	st.markdown(html_temp,unsafe_allow_html=True)
 	
-	page = st.sidebar.selectbox("Escolha uma página", ["Data Scientist","Business"])
+	page = st.sidebar.selectbox("Escolha uma página", ["Visualização do dataset","Fernando","Mario"])
 	
-	if page == "Data Scientist":
+	if page == "Visualização do dataset":
 		st.header("Explore aqui o seu Dataset")
 		visualize_data()
-	elif page == "Business":
-		st.title("Prevendo a quantidade de peças")
+	elif page == "Fernando":
+		st.title('Prevendo a quantidade de peças - Fernando')
 		predicao_fernando()
+	elif page == "Mario":
+		st.title('Prevendo a quantidade de peças - Mário')
+		predicao_mario()
 
 def visualize_data():
 
@@ -299,18 +303,330 @@ def Teste (LinhaProduto,Codigo,CentroCusto,Cliente):
     fig2 = px.line(dfaa , x='semana' , y='quantidade' , category_orders={'semana':suporte} ,
                    template='plotly_dark')
 
-    #fig.show()
-    #fig2.show()
     st.write(fig)
     st.write(fig2)
 
 def predicao_fernando():
-    #st_linhaproduto = st.sidebar.number_input('Linha do produto', value=1, min_value = 0, max_value = 1000, step=1)
-    #st_codigoproduto = st.sidebar.number_input('Código do produto', value=1, min_value = 0, max_value = 1000, step=1)
-    #st_cliente = st.sidebar.number_input('Cliente', value=1, min_value = 0, max_value = 1000, step=1)
-    
-    Teste('Todos','Todos',21320,'Todos')
-    #Teste(st_linhaproduto,st_codigoproduto,21320,st_cliente)
-    
+
+	st_linhaproduto = st.sidebar.number_input('Selecione a linha do produto', value=0, min_value = 0, max_value = 999, step=1)
+	st_codigoproduto = st.sidebar.number_input('Selecione o codigo do produto', value=0, min_value = 0, max_value = 999, step=1)
+	st_cliente = st.sidebar.number_input('Selecione o cliente', value=0, min_value = 0, max_value = 999, step=1)
+
+	#st_linhaproduto = st.sidebar.multiselect('Selecione a linha do produto', [0,1,2,3,"Todos"],0)
+	#st_codigoproduto = st.sidebar.multiselect('Selecione o codigo do produto', 0,[1,2,3,"Todos"],0)
+	#st_cliente = st.sidebar.multiselect('Selecione o cliente', [0,1,2,3,"Todos"],0)
+	
+	Teste('Todos','Todos',21320,'Todos')
+	#Teste(st_linhaproduto,st_codigoproduto,21320,st_cliente)
+def predicao_mario():
+	#IMPORTANDO BIBLIOTECAS
+	import warnings
+	warnings.simplefilter(action='ignore', category=FutureWarning)
+
+	import matplotlib.pyplot as plt
+	from sklearn import metrics
+	from sklearn.model_selection import train_test_split
+	import pandas as pd
+	import matplotlib.pyplot as plt
+	import seaborn as sns
+	import numpy as np
+	import plotly
+	import os
+	#import janitor
+
+
+	from sklearn.metrics import mean_squared_log_error
+	from sklearn.metrics import mean_squared_error
+	from sklearn.metrics import mean_absolute_error as mae
+	from sklearn.ensemble import RandomForestRegressor
+	from sklearn.linear_model import LinearRegression
+	from sklearn.model_selection import cross_val_score
+	from sklearn.model_selection import TimeSeriesSplit 
+	from lightgbm import LGBMRegressor
+	from sklearn.ensemble import RandomForestClassifier
+
+	#from imblearn.over_sampling import SMOTE
+
+	import statsmodels.formula.api as smf            # statistics and econometrics
+	import statsmodels.tsa.api as smt
+	import statsmodels.api as sm
+	import scipy.stats as scs
+
+	from itertools import product                    # some useful functions
+	from tqdm import tqdm_notebook
+
+	import warnings                                  # `do not disturbe` mode
+	warnings.filterwarnings('ignore')
+
+	def mean_absolute_percentage_error(y_true, y_pred): 
+		return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+	pd.set_option('display.max_rows', None)
+	params = {'legend.fontsize': 'x-large',
+			'figure.figsize': (16, 8),
+			'axes.labelsize': 'x-large',
+			'axes.titlesize':'x-large',
+			'xtick.labelsize':'x-large',
+			'ytick.labelsize':'x-large'}
+
+	#%matplotlib inline
+	plt.rcParams.update(params)
+
+	import plotly_express as px
+
+	#IMPORTAÇÃO DO DATASET DE VENDAS
+	vendastelepweek=pd.read_csv("./Datasets/4-VendasTelepWeek-Rev3.csv",sep=',',encoding='Latin1')
+	melt = vendastelepweek.melt(id_vars='Codigo', var_name='Week',value_name='Vendas')
+	melt = melt.sort_values(['Week','Codigo']).reset_index(drop=True)
+	melt['Week'] = melt['Week'].astype(int)
+	melt = melt.sort_values(['Week','Codigo']).reset_index(drop=True)
+
+	#FEATURE ENGINEERING 1: MESES DO ANO
+	Jan = []
+	for w in melt['Week']:
+		if (w<=4) or (w>=52 and w<=56)or (w>=104):
+			jan = 1
+		else:
+			jan = 0
+		Jan.append(jan)
+	melt['Jan'] = Jan
+
+	Fev = []
+	for w in melt['Week']:
+		if (w>=5 and w<=8) or (w>=57 and w<=60):
+			fev = 1
+		else:
+			fev = 0
+		Fev.append(fev)
+	melt['Fev'] = Fev
+
+
+	Mar = []
+	for w in melt['Week']:
+		if (w>=9 and w<=12) or (w>=61 and w<=64):
+			mar = 1
+		else:
+			mar = 0
+		Mar.append(mar)
+	melt['Mar'] = Mar
+
+
+	Abr = []
+	for w in melt['Week']:
+		if (w>=13 and w<=17) or (w>=65 and w<=69):
+			abr = 1
+		else:
+			abr = 0
+		Abr.append(abr)
+	melt['Abr'] = Abr
+
+
+	Mai = []
+	for w in melt['Week']:
+		if (w>=18 and w<=21) or (w>=70 and w<=73):
+			mai = 1
+		else:
+			mai = 0
+		Mai.append(mai)
+	melt['Mai'] = Mai
+
+
+	Jun = []
+	for w in melt['Week']:
+		if (w>=22 and w<=25) or (w>=74 and w<=77):
+			jun = 1
+		else:
+			jun = 0
+		Jun.append(jun)
+	melt['Jun'] = Jun
+
+
+	Jul = []
+	for w in melt['Week']:
+		if (w>=26 and w<=30) or (w>=78 and w<=82):
+			jul = 1
+		else:
+			jul = 0
+		Jul.append(jul)
+	melt['Jul'] = Jul
+
+
+	Ago = []
+	for w in melt['Week']:
+		if (w>=31 and w<=34) or (w>=83 and w<=86):
+			ago = 1
+		else:
+			ago = 0
+		Ago.append(ago)
+	melt['Ago'] = Ago
+
+
+	Set = []
+	for w in melt['Week']:
+		if (w>=35 and w<=38) or (w>=87 and w<=91):
+			set = 1
+		else:
+			set = 0
+		Set.append(set)
+	melt['Set'] = Set
+
+	Out = []
+	for w in melt['Week']:
+		if (w>=39 and w<=43) or (w>=92 and w<=95):
+			out = 1
+		else:
+			out = 0
+		Out.append(out)
+	melt['Out'] = Out
+
+
+	Nov = []
+	for w in melt['Week']:
+		if (w>=44 and w<=47) or (w>=96 and w<=99):
+			nov = 1
+		else:
+			nov = 0
+		Nov.append(nov)
+	melt['Nov'] = Nov
+
+	Dez = []
+	for w in melt['Week']:
+		if (w>=48 and w<=52) or (w>=100 and w<=104):
+			dez = 1
+		else:
+			dez = 0
+		Dez.append(dez)
+	melt['Dez'] = Dez
+
+	#CÁCULO DA MÉDIA E DESVIO PADRÃO DE CADA PRODUTO
+	media = []
+	desvio = []
+
+	for c in melt['Codigo']:
+		med = melt[melt['Codigo'] == c]['Vendas'].mean()
+		media.append(med)
+		
+		dev = melt[melt['Codigo'] == c]['Vendas'].std()
+		desvio.append(dev)
+
+	melt['Media'] = pd.Series(media)
+	melt['Desvio'] = pd.Series(desvio)
+
+	#NORMALIZAÇÃO DA VARIÁVEL TARGET 'VENDAS'
+	melt['VendasStd'] = (melt['Vendas']-melt['Media'])/melt['Desvio']
+
+	#FEATURE ENGINEERING 2: LAG (1 - 4), DIFF (1 - 4)
+	melt['VendasStd_Diff1'] = round(melt.groupby(['Codigo'])['VendasStd'].diff(),4)
+	melt['VendasStd_Diff2'] = round(melt.groupby(['Codigo'])['VendasStd'].diff(2),4)
+	melt['VendasStd_Diff3'] = round(melt.groupby(['Codigo'])['VendasStd'].diff(3),4)
+	melt['VendasStd_Diff4'] = round(melt.groupby(['Codigo'])['VendasStd'].diff(4),4)
+
+	melt['VendasStd_Lag1'] = round(melt.groupby(['Codigo'])['VendasStd'].shift(),4)
+	melt['VendasStd_Lag2'] = round(melt.groupby(['Codigo'])['VendasStd'].shift(2),4)
+	melt['VendasStd_Lag3'] = round(melt.groupby(['Codigo'])['VendasStd'].shift(3),4)
+	melt['VendasStd_Lag4'] = round(melt.groupby(['Codigo'])['VendasStd'].shift(4),4)
+
+	#BASELINE - LASTWEEK - LAG(1)
+	melt['VendasStd_Lag1'] = melt.groupby(['Codigo'])['VendasStd'].shift()
+
+	def rmse (ytrue,ypred):
+		return np.sqrt(mean_squared_error(ytrue,ypred))
+
+	mean_error = []
+	for week in range(84,109):                #31/12/2019 - 24 semanas - 6 meses
+		train = melt[melt['Week'] < week]
+		val = melt[melt['Week'] == week]
+		
+		p = val['VendasStd_Lag1'].values
+		
+		error = mae(val['VendasStd'].values, p)
+		print('Week %d - Error %.5f' % (week, error))
+		mean_error.append(error)
+
+	prod= melt[melt['Codigo']==1]
+	# multiple line plot
+	plt.plot( 'Week', 'VendasStd', data=prod, marker='', markerfacecolor='blue', markersize=12, color='green', linewidth=3,label="True")
+	plt.plot( 'Week', 'VendasStd_Lag1', data=prod, marker='', color='red', linewidth=1,label="Predict",linestyle='dashed')
+	plt.legend()
+	plt.title('BASELINE: Predict = VendasStd_Lag1\nMean Absolute Error = %.5f' % np.mean(mean_error))
+
+
+	#1o MODELO: RANDOM FOREST REGRESSOR
+	def timeseries_train_test_split(X, y, test_size):
+		"""
+			Perform train-test split with respect to time series structure
+		"""
+		
+		# get the index after which test set starts
+		test_index = int(len(X)*(1-test_size))
+		
+		X_train = X.iloc[:test_index]
+		y_train = y.iloc[:test_index]
+		X_test = X.iloc[test_index:]
+		y_test = y.iloc[test_index:]
+		
+		return X_train, X_test, y_train, y_test
+	
+	#COMPARAÇÃO ENTRE OS MODELOS: RFR x LGBM
+	st_produto = st.sidebar.number_input('Escolha um Código de Produto [entre 1 e 201]: ', value=1, min_value = 1, max_value = 201, step=1)
+
+	prod = melt[melt['Codigo'] == st_produto]
+	prod = prod.drop(['Vendas'],axis=1)
+	y = prod.dropna().VendasStd
+	X = prod.dropna().drop(['VendasStd'], axis=1)
+	X_train, X_test, y_train, y_test = timeseries_train_test_split(X, y, test_size=0.3)
+
+	#Random Forest Regressor:
+	rfr = RandomForestRegressor(n_estimators=1000, n_jobs=-1, random_state=0)
+	rfr.fit(X_train, y_train)
+	p_RFR = mdl.predict(X_test)
+	MAE_RFR = mae(y_test, p_RFR)
+
+	X_test_RFR = X_test.copy()
+	X_test_RFR['VendasStd_True'] = pd.Series(y_test)
+	X_test_RFR['PredictStd'] = p_RFR
+	X_test_RFR['MAE'] = abs(X_test_RFR['VendasStd_True'] - X_test_RFR['PredictStd'])
+
+	X_test_RFR['Vendas_True'] = (X_test_RFR['VendasStd_True']*X_test_RFR['Desvio'])+X_test_RFR['Media']
+	X_test_RFR['Predict_True'] = (X_test_RFR['PredictStd']*X_test_RFR['Desvio'])+X_test_RFR['Media']
+	X_test_RFR['MAE_True'] = abs(X_test_RFR['Vendas_True'] - X_test_RFR['Predict_True'])
+
+	#LGBM
+	lgbm = LGBMRegressor(n_estimators=500, learning_rate=0.01)
+	lgbm.fit(X_train, y_train)
+	p_LGBM = lgbm.predict(X_test)
+	MAE_LGBM = mae(y_test,p_LGBM)
+
+	X_test_LGBM = X_test.copy()
+	X_test_LGBM['VendasStd_True'] = pd.Series(y_test)
+	X_test_LGBM['PredictStd'] = p_LGBM
+	X_test_LGBM['MAE'] = abs(X_test_LGBM['VendasStd_True'] - X_test_LGBM['PredictStd'])
+
+	X_test_LGBM['Vendas_True'] = (X_test_LGBM['VendasStd_True']*X_test_LGBM['Desvio'])+X_test_LGBM['Media']
+	X_test_LGBM['Predict_True'] = (X_test_LGBM['PredictStd']*X_test_LGBM['Desvio'])+X_test_LGBM['Media']
+	X_test_LGBM['MAE_True'] = abs(X_test_LGBM['Vendas_True'] - X_test_LGBM['Predict_True'])
+
+	# multiple line plot
+	plt.plot( 'Week', 'Vendas_True', data=X_test_LGBM, marker='', markerfacecolor='blue', markersize=12, color='green', linewidth=3,label="True")
+	plt.plot( 'Week', 'Predict_True', data=X_test_RFR, marker='', color='blue', linewidth=1,label="Predict_RFR",linestyle='dashed')
+	plt.plot( 'Week', 'Predict_True', data=X_test_LGBM, marker='', color='red', linewidth=1,label="Predict_LGBM",linestyle='dashed')
+	plt.legend()
+	plt.title('Codigo %d - LGBM: Predict x True\n MAE RFR: %.5f\nMAE LGBM: %.5f' % (c, MAE_RFR,MAE_LGBM))
+
+	#SHAP VALUES
+	import shap
+	shap.initjs()
+
+	prod = melt[melt['Codigo'] == st_produto]
+	prod = prod.drop(['Vendas'],axis=1)
+	y = prod.dropna().VendasStd
+	X = prod.dropna().drop(['VendasStd'], axis=1)
+	X_train, X_test, y_train, y_test = timeseries_train_test_split(X, y, test_size=0.3)
+
+	import shap
+	shap_values = shap.TreeExplainer(rfr).shap_values(X_train)
+	shap.summary_plot(shap_values, X_train, plot_type="bar")
+	shap.summary_plot(shap_values, X_train)
+
 if __name__ == '__main__':
 	main()
